d3353a7bffb607dbeaabf9ba6242c7cb215c2b88
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-01-19 15:38:05 +0300

    Обертка для компонентов корневого урла

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,10 @@ import AppStyles from './App.module.css';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+import Main from '../Main/Main';
 import AppHeader from '../AppHeader/AppHeader';
-import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
-import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
-import Modal from '../Modal/Modal';
-import OrderDetails from '../OrderDetails/OrderDetails';
-import IngredientDetails from '../IngredientDetails/IngredientDetails';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
-
-import {getIngredients} from '../../services/actions/ingredients';
-import {SHOW_INGREDIENT, HIDE_INGREDIENT} from '../../utils/constants';
 
 import { LoginPage } from '../../pages/login';
 import { RegisterPage } from '../../pages/register';
@@ -27,31 +17,10 @@ import { ProfilePage } from '../../pages/profile';
 function App() {
   const dispatch = useDispatch();
 
-  const [isOrderDetailsOpened, setIsOrderDetailsOpened] = useState(false);
-  const [isIngredientDetailsOpened, setIsIngredientDetailsOpened] = useState(false);
 
-  const { ingredientsByType, ingredientsRequest, ingredientsFailed } = useSelector(state => state.menu);
   const { refreshToken } = useSelector(state => state.user);
   
-  const closeAllModals = () => {
-    dispatch({type: HIDE_INGREDIENT});
-    setIsOrderDetailsOpened(false);
-    setIsIngredientDetailsOpened({isOpened: false});
-  };
   
-  const openOrderDetailsModal = () => {
-    setIsOrderDetailsOpened(true);
-  }
-
-  const openIngredientModal = (e) => {
-    dispatch({type: SHOW_INGREDIENT, id: e.currentTarget.id});
-    setIsIngredientDetailsOpened({isOpened: true});
-  }
-
-  useEffect(() => {
-    dispatch(getIngredients());
-  }, [dispatch]);
-
   return (
     <div className={AppStyles.main + ' m-10'}>
       <AppHeader />      
@@ -59,16 +28,7 @@ function App() {
         <Router>
           <Switch>
             <Route path="/" exact>
-              <ErrorBoundary>
-                {ingredientsFailed && <p>Произошла ошибка</p>}
-                {ingredientsRequest && <p>Загружаем данные</p>}
-                {ingredientsByType && (
-                  <DndProvider backend={HTML5Backend}>
-                    <BurgerIngredients openIngredientModal={openIngredientModal}/>
-                    <BurgerConstructor openOrderDetailsModal={openOrderDetailsModal}/>
-                  </DndProvider>
-                )}
-              </ErrorBoundary>
+              <Main />              
             </Route>
             <Route path="/login">
               { !refreshToken && <LoginPage />}
@@ -96,18 +56,7 @@ function App() {
           </Switch>
         </Router>
       </main>
-      {
-        isOrderDetailsOpened &&
-        <Modal title='' onClose={closeAllModals}>
-            <OrderDetails/>
-        </Modal>
-      }
-      {
-        isIngredientDetailsOpened.isOpened &&
-        <Modal title='Детали ингридиента' onClose={closeAllModals}>
-          <IngredientDetails/>
-        </Modal>
-      }
+      
     </div>
   );
 }
