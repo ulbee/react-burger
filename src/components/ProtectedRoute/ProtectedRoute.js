c9e1e33b33b99d93d2784c0b6b097e7f16c7600e
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-03-04 20:06:49 +0300

    Правка входа пользователя в личный кабинет, если он не был залогинен

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,15 +2,17 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import PropTypes from 'prop-types';
+import { getCookie } from '../../utils/cookie';
 
 function ProtectedRoute({element, isAuthPage, accessFrom}) {
-
+  const accessToken = getCookie('accessToken');
+  const refreshToken = getCookie('token');
   const { isAuthSuccess, isUserLoaded } = useSelector(state => state.user);
 
   const location = useLocation();
   const { from } = location?.state || { from: {pathname: '/'}}
 
-  if (!isUserLoaded) {
+  if (!isUserLoaded && (accessToken || refreshToken)) {
     return <p>Загрузка</p>
 }
   if (isAuthPage && isAuthSuccess) {
